fa704f4f5705c7d057d4fe0e95fa922f36fcbc1e
Pavel <japavel20.com>
2025-01-28 16:35:25 +0600

    Sidebar Clean

--- layout-manager/src/resources/views/components/layouts/partials/footer.blade.php
+++ layout-manager/src/resources/views/components/layouts/partials/footer.blade.php
@@ -1,7 +1,8 @@
-<footer class="footer-area bg-white text-center rounded-top-7" style="position: relative;
-    bottom: 0;
-    width: 100%;       /* Make the footer span the entire width */
-    margin-top: auto;">
+<footer class="footer-area bg-white text-center rounded-top-7" 
+    style="position: relative;
+        bottom: 0;
+        width: 100%; 
+        margin-top: auto;">
     <p class="fs-14">
         © <span class="text-primary-div">Trezo</span> is Proudly
         Owned by
