44704ad5c9e9f8b7860411b61ec4a6ab577f7b70
Pavel <japavel20.com>
2025-02-02 15:18:13 +0600

    reposiotry update

--- layout-manager/src/resources/views/components/layouts/libs/js.blade.php
+++
@@ -1,6 +0,0 @@
-<div id="jsLibContainer">
-    @foreach($jsFiles as $jsFile)
-    <script src="{{ $jsFile }}"></script>
-    @endforeach
-    @stack('js')
-</div>
--- layout-manager/src/resources/views/components/layouts/libs/style.blade.php
+++
@@ -1,9 +0,0 @@
-@foreach($cssFiles as $cssFile)
-    <link href="{{ $cssFile }}" rel="stylesheet" type="text/css" />
-@endforeach
-
-@foreach($imageFiles as $imageFile)
-    <link href="{{ $imageFile }}" rel="icon" type="image/png" />
-@endforeach
-
-@stack('css')
